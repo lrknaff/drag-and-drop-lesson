b28bd9c52e71c7b57998c7109d5f0e628f58aeb2
lrknaff <lacey.knaff@gmail.com>
2016-12-08 20:33:07 -0700

    add functionality and switch to two column

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
-$('.drag-item').on('dragStart', function(e) {
+//transfer data//
+$('.drag-item').on('dragstart', function(e) {
   e.originalEvent.dataTransfer.setData('listItem', $(this).index())
+  console.log('starting')
+})
+.on('drop', function() {
+  console.log('Hello')
 })
 
+//approved//
 $('.kanban-column-approved')
 .on('drop', function(e) {
   e.preventDefault();
   console.log('Dropped!');
   $(this).removeClass('drop-zone-active');
+
   let listItemIndex = e.originalEvent.dataTransfer.getData('listItem');
+  console.log(listItemIndex)
   $(this).append($('.drag-item').eq(listItemIndex))
 })
 .on('dragover', function(e) {
