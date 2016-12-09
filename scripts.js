5ea60f313acb291b1e744a01d93d8641b2391944
lrknaff <lacey.knaff@gmail.com>
2016-12-08 19:00:50 -0700

    able to move cards onto last board on page

--- scripts.js
+++ scripts.js
@@ -1,8 +1,14 @@
+$('.drag-item').on('dragStart', function(e) {
+  e.originalEvent.dataTransfer.setData('listItem', $(this).index())
+})
+
 $('.kanban-column-approved')
 .on('drop', function(e) {
   e.preventDefault();
-  console.log('Dropped!')
-  $(this).removeClass('drop-zone-active')
+  console.log('Dropped!');
+  $(this).removeClass('drop-zone-active');
+  let listItemIndex = e.originalEvent.dataTransfer.getData('listItem');
+  $(this).append($('.drag-item').eq(listItemIndex))
 })
 .on('dragover', function(e) {
    e.preventDefault();
